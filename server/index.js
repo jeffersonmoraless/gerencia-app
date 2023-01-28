d9db8fddcf30bae0d379357e7b5f0c2b569ee64c
Jefferson Morales <105823338+jeffersonmoraless@users.noreply.github.com>
2023-01-28 01:57:48 -0300

    tratamento de email 28/01/2023
    tratamento de verificação do banco de dados para checar se o e-mail já possui cadastro.

--- server/index.js
+++ server/index.js
@@ -17,53 +17,57 @@ app.use(cors());
 app.get('/categoria', (req, res) => {
     let sql = 'select * from categorias'
 
-    db.query(sql,(err,result) => {
-        if(err){
+    db.query(sql, (err, result) => {
+        if (err) {
             console.log(err);
-        }else {
+        } else {
             res.send(result);
-        }})
+        }
+    })
 });
 
 
 app.get('/tipo', (req, res) => {
     let sql = 'select * from tipo'
 
-    db.query(sql,(err,result) => {
-        if(err){
+    db.query(sql, (err, result) => {
+        if (err) {
             console.log(err);
-        }else {
+        } else {
             res.send(result);
-        }})
+        }
+    })
 });
 
 
 app.get('/dividas', (req, res) => {
     let sql = "select descricao , valor, date_format(data,'%d/%m/%Y') data from dividas"
 
-    db.query(sql,(err,result) => {
-        if(err){
+    db.query(sql, (err, result) => {
+        if (err) {
             console.log(err);
-        }else {
+        } else {
             res.send(result);
-        }})
-});
+        }
+    })
+})
 app.get('/dividas/total', (req, res) => {
     let sql = "select sum(valor) valor from dividas"
 
-    db.query(sql,(err,result) => {
-        if(err){
+    db.query(sql, (err, result) => {
+        if (err) {
             console.log(err);
-        }else {
+        } else {
             res.send(result);
-        }})
+        }
+    })
 });
 
-app.post('/cadastrar/despesas', (req, res)=>{
-   
-    const {descricao}= req.body
-    const {valor}= req.body
-    const {data}= req.body
+app.post('/cadastrar/despesas', (req, res) => {
+
+    const { descricao } = req.body
+    const { valor } = req.body
+    const { data } = req.body
     /*const {data_vencimento}= req.body
     const {categoria}= req.body
     const {qtd_parcelas}= req.body
@@ -71,66 +75,78 @@ app.post('/cadastrar/despesas', (req, res)=>{
 
     let sql = "insert into despesas values (?,?,?,?)"
 
-    db.query(sql,[null,descricao,data,valor],(err,result) => {
-        if(err)console.log(err);
-        })
-    });
+    db.query(sql, [null, descricao, data, valor], (err, result) => {
+        if (err) console.log(err);
+    })
+});
 
-app.post('/cadastrar/dividas', (req, res)=>{
-    
-    const {descricao, valor, data, data_vencimento, categoria, qtd_parcelas}= req.body
-    
-    if(qtd_parcelas ===''){
+app.post('/cadastrar/dividas', (req, res) => {
+
+    const { descricao, valor, data, data_vencimento, categoria, qtd_parcelas } = req.body
+
+    if (qtd_parcelas === '') {
         qtd_parcelas = null;
     }
 
     let sql = "insert into dividas values (?,?,?,?,?,?,?)"
 
-    db.query(sql,[null,descricao,valor,data,data_vencimento,categoria,qtd_parcelas],(err,result) => {
-        if(err)console.log(err);
-        })
+    db.query(sql, [null, descricao, valor, data, data_vencimento, categoria, qtd_parcelas], (err, result) => {
+        if (err) console.log(err);
+    })
 })
 
+app.post('/CadastroClientes', (req, res) => {
+    let sql, resposta, { nome, email, password } = req.body
 
-app.post('/CadastroClientes',(req,res)=>{
-
-    let {nome, email, password} = req.body
-    
-    if(!nome){
+    if (!nome) {
         console.log('entrou')
         nome = null
         res.status(422).send('O campo nome é de prenchimento obrigatorio')
-    
-    }
-    if(!email){
+
+    } if (!email) {
         email = null
-        res.status(422).send('O campo email é de prenchimento obrigatorio')           
-    
-    }else if(email === 'verifica se ja tem cadastrado'){
+        res.status(422).send('O campo email é de prenchimento obrigatorio')
 
-        res.status(500).send('O email informado ja possui cadastro')
-    
+    } else {
+
+        sql = 'select email from clientes where email = ?'
+
+        db.query(sql, email, async (err, result) => {
+            if (err) {
+                console.log(err);
+            } else if (result.length >= 1) {
+                if (result[0].email === email) {
+
+                    res.status(500).send('O email informado ja possui cadastro')
+
+                }
+            }
+        })
     }
-    
-    let sql = "insert into clientes values (?,?,?,?)"
-    
-    db.query(sql,[null,nome,email,password],(err,result)=>{
-        if(err){
-            
-            console.log(err)
 
-        }else {
-            
-            console.log('cadastrado com sucesso')
 
-        }
-    })
-    
-    //res.status(200).send('Usuario cadastrado com sucesso')
-    // res.status(500).send('Aconteceu um erro no servidor, tente novamente mais tarde!')
-    
-    
 
+
+sql = "insert into clientes values (?,?,?,?)"
+ 
+db.query(sql,[null,nome,email,password],(err,result)=>{
+    if(err){
+        
+        console.log(err)
+
+    }else {
+        
+        console.log('cadastrado com sucesso')
+
+    }
+})
+ 
+//res.status(200).send('Usuario cadastrado com sucesso')
+// res.status(500).send('Aconteceu um erro no servidor, tente novamente mais tarde!')
+
+ 
+ 
+ 
 })
 
 
@@ -141,6 +157,6 @@ app.post('/CadastroClientes',(req,res)=>{
 
 
 
-app.listen(3500, (res,req)=>{
+app.listen(3500, (res, req) => {
     console.log("rodando porta 3500");
 });
